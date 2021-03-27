f471d4a62a21c32a3d7efab947dc46d3fd28158a
mrsndr <jason.sndr@gmail.com>
2021-03-26 23:28:39 -0600

    Six buttons

---
+++ app/src/main/java/com/example/mysoundboard/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mysoundboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // Sounds from: https://www.pacdv.com/sounds/people_sounds.html
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
